64558e2bc689d5a62c9b36f16e8889b7ef0eebfa
harunorrashid4419 <harunrashid4419@gmail.com>
2022-10-25 11:21:15 +0600

    signle course add

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/courses', (req, res) =>{
     res.send(courses)
  })
 
+ app.get("/courses/:id", (req, res) => {
+    const id = req.params.id;
+    const course = courses.find((n) => n.id === id);
+    res.send(course);
+ });
+
 app.listen(port, () =>{
     console.log(`server side is running, ${port}`)
 })
