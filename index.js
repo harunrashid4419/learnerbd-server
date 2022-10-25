c5a3d90eca234245aa3b4b8db593458a4d3fb203
harunorrashid4419 <harunrashid4419@gmail.com>
2022-10-25 11:19:16 +0600

    all courses add

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const cors = require ('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json');
+
 app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('learning platform server is running')
 })
+
+app.get('/courses', (req, res) =>{
+    res.send(courses)
+ })
 
 app.listen(port, () =>{
     console.log(`server side is running, ${port}`)
